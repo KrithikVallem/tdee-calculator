702287db993c5852db5c71b2bc579b47140bcbf5
vallemkr <vallemkr@umich.edu>
2020-03-07 16:41:29 -0500

    finished fixing the input stuff, now need to work on actual formula calculation

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
-restoreFormFieldInputs(inputs) {
-    alert("hi");
+
+
+function validateFormInputs(inputs) {
+    inputs.age = parseInt(document.querySelector("#age").value);
+    inputs.weight = parseInt(document.querySelector("#weight").value);
+    inputs.height = parseInt(document.querySelector("#height").value);
+    inputs.bodyFatPercent = parseInt(document.querySelector("#bodyFatPercent").value);
+    inputs.bodyFatEntered = false;
+
+    if (!isNaN(inputs.bodyFatPercent)) {
+        inputs.bodyFatEntered = true;
+
+        if (isNaN(inputs.bodyFatPercent) || inputs.bodyFatPercent < 0 || inputs.bodyFatPercent > 100) {
+            alert("Please enter a valid body fat percentage!");
+            return false;
+        }
+    }
+
+    if (isNaN(inputs.age) || inputs.age === "" || inputs.age < 0) {    
+        alert("Please enter a valid age!");
+        return false;
+    }
+
+    if (isNaN(inputs.weight) || inputs.weight === "" || inputs.weight < 0) {    
+        alert("Please enter a valid weight!");
+        return false;
+    }
+
+    if (isNaN(inputs.height) || inputs.height === "" || inputs.height < 0) {    
+        alert("Please enter a valid height!");
+        return false;
+    }
+
+
+    
+
+    return true;
 }
 
 function formSubmit() {
-
     const inputs = {
-        //gender: document.querySelector("#gender").value,
-        age: document.querySelector("#age").value,
-        weight: document.querySelector("#weight").value,
-        height: document.querySelector("#height").value,
-        activityLevel: document.querySelector("#activityLevel").value,
-        //bodyFatPercent: document.querySelector("#bodyFatPercent").value,
+        age: -1,
+        weight: -1,
+        height: -1,
+
+        bodyFatEntered: false,
+        bodyFatPercent: -1,
+
+        gender: "M",
+        activityLevel: "S",
     };
 
-    restoreFormFieldInputs(inputs);
+    if (!validateFormInputs(inputs)) {
+        return;
+    }
 
-    alert(inputs.age);
+    
 }
 
 document.querySelector("#submitBtn").addEventListener("click", formSubmit);
 
-document.querySelector("table").addEventListener("mouseover", restoreFormFieldInputs);
+
 
 
